ad8a967ac4660bdadbfea08705e2712105e2974d
skrussell <russell@samknows.com>
2017-03-01 10:32:39 +0000

    Added 2ndquadrant Postgres 9.4 with DBR repo & gpg-key

---
+++ manifests/repo/2ndq-bdr94.pp
@@ -0,0 +1,32 @@
+# = Class: yum::repo::2ndq-bdr94
+#
+# This class installs the postgresql 9.4 repo from 2ndquadrant that includes the
+# bi-directional replication patches.
+#
+class yum::repo::2ndq-bdr94 {
+  yum::managed_yumrepo { 'postgresql-bdr94-2ndquadrant':
+    descr         => 'PostgreSQL 9.4 with BDR for RHEL $releasever - $basearch',
+    baseurl       => 'http://packages.2ndquadrant.com/postgresql-bdr94-2ndquadrant/yum/redhat-$releasever-$basearch',
+    enabled       => 1,
+    gpgcheck      => 1,
+    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-2NDQ-BDR-94',
+    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-2NDQ-BDR-94'
+  }
+  yum::managed_yumrepo { 'postgresql-bdr94-2ndquadrant-source':
+    descr          => 'PostgreSQL 9.4 with BDR for RHEL $releasever - source',
+    baseurl        => 'http://packages.2ndquadrant.com/postgresql-bdr94-2ndquadrant/yum/redhat-$releasever',
+    enabled        => 0,
+    gpgcheck       => 1,
+	failovermethod => 'priority'
+    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-2NDQ-BDR-94',
+    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-2NDQ-BDR-94'
+  }
+  yum::managed_yumrepo { 'postgresql-bdr94-2ndquadrant-testing':
+    descr         => 'PostgreSQL 9.4 with BDR testing for RHEL $releasever - $basearch',
+    baseurl       => 'http://packages.2ndquadrant.com/postgresql-bdr94-2ndquadrant/yum-testing/redhat-$releasever-$basearch',
+    enabled       => 0,
+    gpgcheck      => 1,
+    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-2NDQ-BDR-94',
+    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-2NDQ-BDR-94'
+  }
+}
