07151646c3ee92f7b695bd0ac4f51bb297258ff2
skrussell <russell@samknows.com>
2017-09-07 09:32:35 +0100

    Add Erlang Solutions erlang repository

---
+++ manifests/repo/erlang.pp
@@ -0,0 +1,14 @@
+# = Class: yum::repo::erlang
+#
+# This class installs the erlang repository from Erlang Solutions
+#
+class yum::repo::erlang {
+  yum::managed_yumrepo { 'erlang':
+    descr         => 'Erlang solutions official repository for RHEL and Cent OS - $basearch',
+    baseurl       => 'http://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch',
+    enabled       => 1,
+    gpgcheck      => 1,
+    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-erlang',
+    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-erlang'
+  }
+}
