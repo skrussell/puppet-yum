aa57dd7b6c7813f67d9f3b046357bf4c598be848
skrussell <russell@samknows.com>
2017-09-07 09:40:24 +0100

    Add priority to erlang

--- manifests/repo/erlang.pp
+++ manifests/repo/erlang.pp
@@ -4,11 +4,13 @@
 #
 class yum::repo::erlang {
   yum::managed_yumrepo { 'erlang':
-    descr         => 'Erlang solutions official repository for RHEL and Cent OS - $basearch',
-    baseurl       => 'http://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch',
-    enabled       => 1,
-    gpgcheck      => 1,
-    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-erlang',
-    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-erlang'
+    descr          => 'Erlang solutions official repository for RHEL and Cent OS - $basearch',
+    baseurl        => 'http://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch',
+    enabled        => 1,
+	failovermethod => 'priority'
+	priority       => 1,
+    gpgcheck       => 1,
+    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-erlang',
+    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-erlang'
   }
 }
