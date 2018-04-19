1a871cf992d07ef13a1760fb4da953c96c1c303e
skrussell <russell@samknows.com>
2018-04-19 18:24:00 +0100

    Add package name argument to versionlock

--- manifests/versionlock.pp
+++ manifests/versionlock.pp
@@ -1,21 +1,28 @@
 # = Define yum::versionlock
 #
 define yum::versionlock (
-	Enum['present','absent'] $ensure = 'present'
+	Enum['present','absent'] $ensure = 'present',
+	Optional[String] $package = undef
 ) {
 	include ::yum::plugin::versionlock
 
-	$locked_check = "yum -C versionlock list | grep -q ${name}"
+	if ($package) {
+		$use_package_name = $package
+	} else {
+		$use_package_name = $name
+	}
+
+	$locked_check = "yum -C versionlock list | grep -q ${use_package_name}"A
 
 	if ($ensure == 'present') {
 		exec { "add-yum-lock-${name}":
-			command => "yum -C versionlock add ${name}",
+			command => "yum -C versionlock add ${use_package_name}",
 			unless  => $locked_check,
 			require => Class['yum::plugin::versionlock'],
 		}
 	} else {
 		exec { "remove-yum-lock-${name}":
-			command => "yum -C versionlock delete ${name}",
+			command => "yum -C versionlock delete ${use_package_name}",
 			onlyif  => $locked_check,
 			require => Class['yum::plugin::versionlock'],
 		}
