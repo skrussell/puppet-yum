f2a4a6e6331d97b126a066139a27042df2ba2c02
skrussell <russell@samknows.com>
2018-10-02 09:38:56 +0100

    Make the tideway repo update run a force clean

--- manifests/repo/tideways.pp
+++ manifests/repo/tideways.pp
@@ -3,13 +3,20 @@
 # This class installs the tideways repo
 #
 class yum::repo::tideways {
-  yum::managed_yumrepo { 'tideways':
-    descr         => 'Tideways RPM repository for Redhat/Fedora/CentOS',
-    baseurl       => 'https://s3-eu-west-1.amazonaws.com/tideways/rpm',
-    enabled       => 1,
-    gpgcheck      => 1,
-    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-tideways',
-    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-tideways',
-    priority      => 1,
-  }
+	$repo_name = 'tideways'
+	yum::managed_yumrepo { $repo_name:
+		descr         => 'Tideways RPM repository for Redhat/Fedora/CentOS',
+		baseurl       => 'https://s3-eu-west-1.amazonaws.com/tideways/rpm',
+		enabled       => 1,
+		gpgcheck      => 1,
+		gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-tideways',
+		gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-tideways',
+		priority      => 1,
+		notify        => Exec[ "yum_clean_${repo_name}_cache" ]
+	}
+
+	exec { "yum_clean_${repo_name}_cache":
+		command     => "yum clean all --enablerepo=${repo_name} --disablerepo='*'",
+		refreshonly => true
+	}
 }
