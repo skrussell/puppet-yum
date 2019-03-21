cf5b604f91637640fdb8df96718cc586af92f903
skrussell <russell@samknows.com>
2019-03-21 08:55:58 +0000

    Force removal of the old rabbitmq GPG key file

--- manifests/repo/rabbitmq_server.pp
+++ manifests/repo/rabbitmq_server.pp
@@ -3,6 +3,13 @@
 # This class installs the RabbitMQ Server repo
 #
 class yum::repo::rabbitmq_server {
+	$gpg_key_filename = 'RPM-GPG-KEY-rabbitmq'
+	$gpg_key_file     = "/etc/pki/rpm-gpg/${gpg_key_filename}"
+	exec { 'remove_old_gpg_key':
+		command => "rm -f ${gpg_key_file})",
+		onlyif  => "test \"$(sha1sum < ${gpg_key_file})\" = \"8f6e2a1aacca3cbc5a900fce1119ae4310f0c8c1  -\"",
+		before  => Yum::Managed_yumrepo[ 'rabbitmq_rabbitmq-server', 'rabbitmq_rabbitmq-server-source' ]
+	}
 	yum::managed_yumrepo { 'rabbitmq_rabbitmq-server':
 		descr           => 'rabbitmq_rabbitmq-server',
 		baseurl         => 'https://packagecloud.io/rabbitmq/rabbitmq-server/el/$releasever/$basearch',
@@ -11,8 +18,8 @@ class yum::repo::rabbitmq_server {
 		priority        => 1,
 		gpgcheck        => 0,
 		repo_gpgcheck   => 1,
-		gpgkey          => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rabbitmq',
-		gpgkey_source   => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-rabbitmq',
+		gpgkey          => "file://${gpg_key_file}",
+		gpgkey_source   => "puppet:///modules/yum/rpm-gpg/${gpg_key_filename}",
 		metadata_expire => 300,
 		sslverify       => 1,
 		sslcacert       => '/etc/pki/tls/certs/ca-bundle.crt'
@@ -25,8 +32,8 @@ class yum::repo::rabbitmq_server {
 		priority        => 1,
 		gpgcheck        => 0,
 		repo_gpgcheck   => 1,
-		gpgkey          => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rabbitmq',
-		gpgkey_source   => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-rabbitmq',
+		gpgkey          => "file://${gpg_key_file}",
+		gpgkey_source   => "puppet:///modules/yum/rpm-gpg/${gpg_key_filename}",
 		metadata_expire => 300,
 		sslverify       => 1,
 		sslcacert       => '/etc/pki/tls/certs/ca-bundle.crt'
