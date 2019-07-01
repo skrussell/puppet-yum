bfc365fde60d9e1d2a24f36dbef2c0b2754f848d
skrussell <russell@samknows.com>
2019-07-01 15:51:58 +0100

    Clean-up the dell omsa module

--- manifests/repo/dell_omsa.pp
+++ manifests/repo/dell_omsa.pp
@@ -5,24 +5,19 @@
 class yum::repo::dell_omsa (
 	Enum['present','absent'] $ensure = 'present'
 ) {
-  yum::managed_yumrepo { 'dell-omsa-indep':
-		ensure         => $ensure,
-    descr          => 'Dell OMSA repository - Hardware independent',
-    enabled        => 1,
-    gpgcheck       => 1,
-    mirrorlist     => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&dellsysidpluginver=$dellsysidpluginver',
-    failovermethod => 'priority',
-    gpgkey         => 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios',
+  yum::managed_yumrepo { 
+		default:
+			ensure         => $ensure,
+			enabled        => 1,
+			gpgcheck       => 1,
+			failovermethod => 'priority',
+			gpgkey         => [ 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell',
+													'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios' ];
+		'dell-omsa-indep':
+			descr      => 'Dell OMSA repository - Hardware independent',
+			mirrorlist => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&dellsysidpluginver=$dellsysidpluginver';
+		'dell-omsa-specific':
+			descr      => 'Dell OMSA repository - Hardware specific',
+			mirrorlist => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&sys_ven_id=$sys_ven_id&sys_dev_id=$sys_dev_id&dellsysidpluginver=$dellsysidpluginver';
   }
-
-  yum::managed_yumrepo { 'dell-omsa-specific':
-		ensure         => $ensure,
-    descr          => 'Dell OMSA repository - Hardware specific',
-    enabled        => 1,
-    gpgcheck       => 1,
-    mirrorlist     => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&sys_ven_id=$sys_ven_id&sys_dev_id=$sys_dev_id&dellsysidpluginver=$dellsysidpluginver',
-    failovermethod => 'priority',
-    gpgkey         => 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios',
-  }
-
 }
