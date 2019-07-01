4466b2b0f01af4d759bcc4105cc5919323815e43
skrussell <russell@samknows.com>
2019-07-01 16:07:23 +0100

    More updates to the dell repo

--- manifests/repo/dell_system_update.pp
+++ manifests/repo/dell_system_update.pp
@@ -10,17 +10,18 @@ class yum::repo::dell_system_update (
 			autokeyimport  => 'yes',
 			ensure         => $ensure,
 			enabled        => 1,
-			failovermethod => 'priority',
 			file_name      => 'dell-system-update',
 			gpgcheck       => 1,
 			gpgkey         => [
-				'https://linux.dell.com/repo/hardware/dsu/public.key',
-				'https://linux.dell.com/repo/hardware/dsu/public_gpg3.key'
+				'https://linux.dell.com/repo/pgp_pubkeys/0x756ba70b1019ced6.asc',
+				'https://linux.dell.com/repo/pgp_pubkeys/0x1285491434D8786F.asc',
+				'https://linux.dell.com/repo/pgp_pubkeys/0xca77951d23b66a9d.asc'
 			];
 		'dell-system-update_independent':
-			mirrorlist => 'https://linux.dell.com/repo/hardware/dsu/os_independent/'
+			exclude => 'dell-system-update*.i386',
+			baseurl => 'https://linux.dell.com/repo/hardware/dsu/os_independent/';
 		'dell-system-update_dependent':
-			mirrorlist => 'https://linux.dell.com/repo/hardware/dsu/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1'
+			mirrorlist => 'https://linux.dell.com/repo/hardware/dsu/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1';
   }
 
 }
