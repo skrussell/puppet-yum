2cce4107f88758b9bac0a0d35fafaa54a8716dc0
skrussell <russell@samknows.com>
2019-07-18 15:04:56 +0100

    Re-write the nginx module to support the stable/mainline streams

--- manifests/repo/nginx.pp
+++ manifests/repo/nginx.pp
@@ -2,15 +2,28 @@
 #
 # This class installs the nginx repo
 #
-class yum::repo::nginx {
-  $osver = split($::operatingsystemrelease, '[.]')
+class yum::repo::nginx (
+	Enum['stable','mainline'] $stream = 'stable'
+) {
+	if ($facts['os']['family'] == 'RedHat' and $facts['os']['name'] == 'RedHat') {
+		$use_os_path = 'rhel'
+	} elsif ($facts['os']['family'] == 'RedHat' and $facts['os']['name'] == 'CentOS') {
+		$use_os_path = 'centos'
+	} else {
+		fail("OS is currently not supported")
+	}
+
+	$baseurl = $stream ? {
+		'stable' => "http://nginx.org/packages/${use_os_path}/\$releasever/\$basearch/",
+		default  => "http://nginx.org/packages/${stream}/${use_os_path}/\$releasever/\$basearch/"
+	}
 
   yum::managed_yumrepo { 'nginx':
-    descr    => 'Nginx official release packages',
-    baseurl  => "http://nginx.org/packages/rhel/${osver[0]}/\$basearch/",
+    descr    => "nginx ${stream} repo"
+    baseurl  => $baseurl,
     enabled  => 1,
-    gpgcheck => 0,
+    gpgcheck => 1,
+		gpgkey   => 'https://nginx.org/keys/nginx_signing.key'
     priority => 1,
   }
-
 }
