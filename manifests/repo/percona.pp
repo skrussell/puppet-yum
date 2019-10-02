f3ac570f199097eb66ad286014160ed09196e638
skrussell <russell@samknows.com>
2019-10-02 15:04:09 +0100

    Update the percona repository

--- manifests/repo/percona.pp
+++ manifests/repo/percona.pp
@@ -3,13 +3,19 @@
 # This class installs the Percona RPM Repository
 #
 class yum::repo::percona {
-  yum::managed_yumrepo { 'percona':
-    descr          => 'Percona RPM Repository (http://www.percona.com/percona-lab.html)',
-    baseurl        => 'http://repo.percona.com/centos/$releasever/os/$basearch/',
-    enabled        => 1,
-    gpgcheck       => 1,
-    failovermethod => 'priority',
-    gpgkey         => 'http://www.percona.com/downloads/RPM-GPG-KEY-percona',
-    priority       => 1,
-  }
+	$base_url = 'http://repo.percona.com/yum/release'
+	if ($facts['os']['name'] == 'CentOS') {
+		$use_base_url = "${base_url}/${downcase($facts['os']['name'])}"
+	} else {
+		$use_base_url = $base_url
+	}
+	yum::managed_yumrepo { 'percona':
+		descr          => 'Percona RPM Repository (http://www.percona.com/percona-lab.html)',
+		baseurl        => "${use_base_url}/$releasever/os/$basearch/",
+		enabled        => 1,
+		gpgcheck       => 1,
+		failovermethod => 'priority',
+		gpgkey         => 'https://repo.percona.com/yum/PERCONA-PACKAGING-KEY',
+		priority       => 1,
+	}
 }
