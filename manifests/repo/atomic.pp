e3a04125f9e461e545be4806a7db2dff0d79892f
skrussell <russell@samknows.com>
2019-10-21 16:48:12 +0100

    Add mirror_url support to atomicorp repo

--- manifests/repo/atomic.pp
+++ manifests/repo/atomic.pp
@@ -2,18 +2,28 @@
 #
 # This class installs the atomic repo
 #
-class yum::repo::atomic {
-  $os = downcase($facts['os']['name'])
+class yum::repo::atomic (
+	Optional[Stdlib::Httpurl] $mirror_url = undef
+) {
+	$os = downcase($facts['os']['name'])
 
-  yum::managed_yumrepo { 'atomic':
-    descr         => 'CentOS / Red Hat Enterprise Linux $releasever - atomicrocketturtle.com',
-    mirrorlist    => "http://updates.atomicorp.com/channels/mirrorlist/atomic/${os}-\$releasever-\$basearch",
-    enabled       => 1,
-    gpgcheck      => 1,
-#    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY.art',
-    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY.atomicorp',
-    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY.atomicorp',
-    priority      => 1,
-    exclude       => 'nmap-ncat',
-  }
+	if ($mirror_url) {
+		$use_baseurl    = "${mirror_url}/${os}/\$releasever/\$basearch"
+		$use_mirrorlist = undef
+	} else {
+		$use_baseurl    = undef
+		$use_mirrorlist = "http://updates.atomicorp.com/channels/mirrorlist/atomic/${os}-\$releasever-\$basearch"
+	}
+
+	yum::managed_yumrepo { 'atomic':
+		descr         => 'CentOS / Red Hat Enterprise Linux $releasever - atomicrocketturtle.com',
+		baseurl       => $use_baseurl,
+		mirrorlist    => $use_mirrorlist,
+		enabled       => 1,
+		gpgcheck      => 1,
+		gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY.atomicorp',
+		gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY.atomicorp',
+		priority      => 1,
+		exclude       => 'nmap-ncat',
+	}
 }
