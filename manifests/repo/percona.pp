fd40b49b340cc7b2a15f361335a5d12ef20b9d60
skrussell <russell@samknows.com>
2019-10-28 16:10:10 +0000

    Add mirror_url and CentOS 8 support to the percona repo

--- manifests/repo/percona.pp
+++ manifests/repo/percona.pp
@@ -2,20 +2,38 @@
 #
 # This class installs the Percona RPM Repository
 #
-class yum::repo::percona {
-	$base_url = 'http://repo.percona.com/yum/release'
-	if ($facts['os']['name'] == 'CentOS') {
-		$use_base_url = "${base_url}/${downcase($facts['os']['name'])}"
+class yum::repo::percona (
+	String $mirror_url
+) {
+	if ($mirror_url) {
+		$use_url = $mirror_url
+		$use_gpgsrc = 'puppet:///modules/yum/rpm-gpg/PERCONA-PACKAGING-KEY'
+		$use_gpgkey = 'file:///etc/pki/rpm-gpg/PERCONA-PACKAGING-KEY'
 	} else {
-		$use_base_url = $base_url
+		$base_url = 'http://repo.percona.com/yum/release'
+		if ($facts['os']['name'] == 'CentOS' and versioncmp($facts['os']['release']['major'], '7' <=)) {
+			$use_base_url = "${base_url}/${downcase($facts['os']['name'])}"
+		} else {
+			$use_base_url = $base_url
+		}
+
+		if (versioncmp($facts['os']['release']['major'], '8') >= 0) {
+			$use_url = "${use_base_url}/\$releasever/RPMS/\$basearch/"
+		} else {
+			$use_url = "${use_base_url}/\$releasever/os/\$basearch/"
+		}
+		$use_gpgsrc = undef
+		$use_gpgkey = 'https://repo.percona.com/yum/PERCONA-PACKAGING-KEY'
 	}
+
 	yum::managed_yumrepo { 'percona':
 		descr          => 'Percona RPM Repository (http://www.percona.com/percona-lab.html)',
 		baseurl        => "${use_base_url}/\$releasever/os/\$basearch/",
 		enabled        => 1,
 		gpgcheck       => 1,
 		failovermethod => 'priority',
-		gpgkey         => 'https://repo.percona.com/yum/PERCONA-PACKAGING-KEY',
+		gpgkey_source  => $use_gpgsrc,
+		gpgkey         => $use_gpgkey,
 		priority       => 1,
 	}
 }
