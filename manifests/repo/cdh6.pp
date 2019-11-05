51468dd37657c92530503f7f0c310d007a526df8
skrussell <russell@samknows.com>
2019-11-05 14:17:21 +0000

    Add missing GPL Extras repo for cloudera 6

--- manifests/repo/cdh6.pp
+++ manifests/repo/cdh6.pp
@@ -6,7 +6,8 @@
 # @param baseurl_extras An overriding URL to use (must be a full Yum repository URL for the gplextra6 packages)
 class yum::repo::cdh6 (
 	Pattern[/\A6(\.\d\d?){0,2}\z/] $version = '6.0.0',
-	Optional[Stdlib::HTTPurl] $baseurl = undef
+	Optional[Stdlib::HTTPurl] $baseurl = undef,
+	Optional[Stdlib::HTTPurl] $baseurl_extras = undef
 ) {
 	unless (($baseurl and $baseurl) or $facts['os']['hardware'] == 'x86_64') {
 		fail('Only x86_84 packages are hosted in the cloudera repository')
@@ -16,9 +17,26 @@ class yum::repo::cdh6 (
 	} else {
 		$use_baseurl = "https://archive.cloudera.com/cdh6/${version}/redhat${facts['os']['release']['major']}/yum/"
 	}
+	if ($baseurl_extras) {
+		$use_baseurl_extras = $baseurl_extras
+	} else {
+		$use_baseurl_extras = "https://archive.cloudera.com/gplextras6/${version}/redhat${facts['os']['release']['major']}/yum/"
+	}
+
 	yum::managed_yumrepo { 'cloudera-cdh6':
 		descr          => "Cloudera's Distribution for Hadoop, Version 6",
 		baseurl        => $use_baseurl,
+		enabled        => 1,
+		gpgcheck       => 1,
+		gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-cloudera',
+		gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-cloudera',
+		priority       => 20,
+		failovermethod => 'priority',
+	}
+
+	yum::managed_yumrepo { 'cloudera-gplextras6':
+		descr          => "Cloudera's GPLExtras, Version 6",
+		baseurl        => $use_baseurl_extras,
 		enabled        => 1,
 		gpgcheck       => 1,
 		gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-cloudera',
