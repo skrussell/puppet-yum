9ee9816429144542ab940039c258ac35a6253c08
skrussell <russell@samknows.com>
2019-11-19 09:13:03 +0000

    Expand the erlang mirror config

--- manifests/repo/erlang.pp
+++ manifests/repo/erlang.pp
@@ -2,15 +2,26 @@
 #
 # This class installs the erlang repository from Erlang Solutions
 #
-class yum::repo::erlang {
-  yum::managed_yumrepo { 'erlang':
-    descr          => 'Erlang solutions official repository for RHEL and Cent OS - $basearch',
-    baseurl        => 'http://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch',
-    enabled        => 1,
-	failovermethod => 'priority',
-	priority       => 1,
-    gpgcheck       => 1,
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-erlang',
-    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-erlang'
-  }
+class yum::repo::erlang (
+	Enum['present','absent'] $ensure = 'present',
+	Optional[Stdlib::Httpurl] $mirror_url = undef,
+	Optional[String] $exclude
+) {
+	if ($mirror_url) {
+		$use_baseurl = $mirror_url
+	} else {
+		$use_baseurl = 'http://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch'
+	}
+	yum::managed_yumrepo { 'erlang':
+		ensure         => $ensure,
+		descr          => 'Erlang solutions official repository for RHEL and Cent OS - $basearch',
+		baseurl        => $use_baseurl,
+		enabled        => 1,
+		failovermethod => 'priority',
+		priority       => 1,
+		gpgcheck       => 1,
+		gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-erlang',
+		gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-erlang',
+		exclude        => $exclude
+	}
 }
