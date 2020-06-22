0116913b5f62f04ff7ac50c6843432aafd939098
skrussell <russell@samknows.com>
2020-06-22 11:10:06 +0100

    Make sure the 'updates' repo has been removed as it is causing issues

--- manifests/repo/centos8.pp
+++ manifests/repo/centos8.pp
@@ -24,68 +24,56 @@ class yum::repo::centos8 (
       '^(?:https?|ftp):\/\/[\da-zA-Z-][\da-zA-Z\.-]*\.[a-zA-Z]{2,6}\.?(?:\/[\w\.~-]*)*$',
       "\$mirror must be a Clean URL with no query-string, a fully-qualified hostname and no trailing slash. Recieved '${mirror_url}'"
     )
-    $baseurl_base = "${mirror_url}/\$releasever/os/\$basearch/"
-    $baseurl_updates = "${mirror_url}/\$releasever/updates/\$basearch/"
+    $baseurl_base = "${mirror_url}/\$releasever/BaseOS/\$basearch/"
     $baseurl_extras = "${mirror_url}/\$releasever/extras/\$basearch/"
     $baseurl_centosplus = "${mirror_url}/\$releasever/centosplus/\$basearch/"
     $mirrorlist_base = undef
-    $mirrorlist_updates = undef
     $mirrorlist_extras = undef
     $mirrorlist_centosplus = undef
   } else {
     $baseurl_base = undef
-    $baseurl_updates = undef
     $baseurl_extras = undef
     $baseurl_centosplus = undef
-    $mirrorlist_base = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra'
-    $mirrorlist_updates = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra'
+    $mirrorlist_base = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=BaseOS&infra=$infra'
     $mirrorlist_extras = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra'
     $mirrorlist_centosplus = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra'
   }
 
   yum::managed_yumrepo { 'base':
-    descr          => 'CentOS-$releasever - Base',
-    baseurl        => $baseurl_base,
-    mirrorlist     => $mirrorlist_base,
-    failovermethod => 'priority',
-    enabled        => 1,
-    gpgcheck       => 1,
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-8',
-    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-CentOS-8',
-    priority       => 2,
+    descr         => 'CentOS-$releasever - Base',
+    baseurl       => $baseurl_base,
+    mirrorlist    => $mirrorlist_base,
+    enabled       => 1,
+    gpgcheck      => 1,
+    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-8',
+    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-CentOS-8',
+    priority      => 2,
   }
 
   yum::managed_yumrepo { 'updates':
-    descr          => 'CentOS-$releasever - Updates',
-    baseurl        => $baseurl_updates,
-    mirrorlist     => $mirrorlist_updates,
-    failovermethod => 'priority',
-    enabled        => 1,
-    gpgcheck       => 1,
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-8',
-    priority       => 2,
+    ensure => 'absent'
   }
 
   yum::managed_yumrepo { 'extras':
-    descr          => 'CentOS-$releasever - Extras',
-    baseurl        => $baseurl_extras,
-    mirrorlist     => $mirrorlist_extras,
-    failovermethod => 'priority',
-    enabled        => 1,
-    gpgcheck       => 1,
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-8',
-    priority       => 2,
+    descr         => 'CentOS-$releasever - Extras',
+    baseurl       => $baseurl_extras,
+    mirrorlist    => $mirrorlist_extras,
+    enabled       => 1,
+    gpgcheck      => 1,
+    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-8',
+    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-CentOS-8',
+    priority      => 2,
   }
 
   yum::managed_yumrepo { 'centosplus':
-    descr          => 'CentOS-$releasever - Centosplus',
-    baseurl        => $baseurl_centosplus,
-    mirrorlist     => $mirrorlist_centosplus,
-    failovermethod => 'priority',
-    enabled        => 1,
-    gpgcheck       => 1,
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-8',
-    priority       => 3,
+    descr         => 'CentOS-$releasever - Centosplus',
+    baseurl       => $baseurl_centosplus,
+    mirrorlist    => $mirrorlist_centosplus,
+    enabled       => 1,
+    gpgcheck      => 1,
+    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-8',
+    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-CentOS-8',
+    priority      => 3,
   }
 
 }
