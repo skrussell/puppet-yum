9c6834502a19cec1ee14bfd39b178b1f9d3e3bbf
skrussell <russell@samknows.com>
2021-01-11 13:46:08 +0000

    Add a priority to the endpoint repo

--- manifests/repo/endpoint.pp
+++ manifests/repo/endpoint.pp
@@ -32,7 +32,8 @@ class yum::repo::endpoint (
 		enabled        => 1,
 		gpgcheck       => 1,
 		gpgkey         => "file:///etc/pki/rpm-gpg/${gpg_filename}" ,
-		gpgkey_source  => "puppet:///modules/yum/rpm-gpg/${gpg_filename}"
+		gpgkey_source  => "puppet:///modules/yum/rpm-gpg/${gpg_filename}",
+		priority       => 1
 	}
 
 }
