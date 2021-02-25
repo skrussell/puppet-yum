cd9618c8fc150e32d7817ccfc8e7d450ea26ff9d
Russell Knighton <russell@samknows.com>
2021-02-25 16:14:46 +0000

    Fix gpg key autoimporting, and enable it for the puppet repos

--- manifests/repo/puppet6.pp
+++ manifests/repo/puppet6.pp
@@ -3,36 +3,37 @@
 # This class installs the puppet6 repo
 #
 class yum::repo::puppet6 (
-  $host_server = 'yum.puppetlabs.com',
-  $baseurl     = '',
-  $priority    = 99,
+	$host_server = 'yum.puppetlabs.com',
+	$baseurl     = '',
+	$priority    = 99,
 ) {
 	$version = 6
-  $osver = $::operatingsystem ? {
-    'XenServer' => [ '5' ],
-    default     => split($::operatingsystemrelease, '[.]')
-  }
-  $release = $::operatingsystem ? {
-    /(?i:Centos|RedHat|Scientific|CloudLinux|XenServer|OracleLinux)/ => $osver[0],
-    default                                              => '6',
-  }
+	$osver = $::operatingsystem ? {
+		'XenServer' => [ '5' ],
+		default     => split($::operatingsystemrelease, '[.]')
+	}
+	$release = $::operatingsystem ? {
+		/(?i:Centos|RedHat|Scientific|CloudLinux|XenServer|OracleLinux)/ => $osver[0],
+		default                                                          => '6',
+	}
 
-  $real_baseurl = $baseurl ? {
-	''      => "http://${host_server}/puppet${version}/el/${release}/\$basearch",
-    default => $baseurl,
-  }
+	$real_baseurl = $baseurl ? {
+		''      => "http://${host_server}/puppet${version}/el/${release}/\$basearch",
+		default => $baseurl,
+	}
 
-  yum::managed_yumrepo { "puppet${version}":
-    descr          => "Puppet ${version} Repository el ${release} - \$basearch",
-    baseurl        => $real_baseurl,
-    enabled        => 1,
-    gpgcheck       => 1,
-    gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet${version}-release",
-    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-puppet',
-    gpgkey_name    => "RPM-GPG-KEY-puppet${version}-release",
-    priority       => $priority,
-  }
-  -> file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet${version}":
-    ensure => 'absent'
-  }
+	yum::managed_yumrepo { "puppet${version}":
+		descr         => "Puppet ${version} Repository el ${release} - \$basearch",
+		baseurl       => $real_baseurl,
+		enabled       => 1,
+		gpgcheck      => 1,
+		autokeyimport => 'yes',
+		gpgkey        => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet${version}-release",
+		gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-puppet',
+		gpgkey_name   => "RPM-GPG-KEY-puppet${version}-release",
+		priority      => $priority,
+	}
+	-> file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet${version}":
+		ensure => 'absent'
+	}
 }
